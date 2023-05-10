38b9e74c801a69c13597cfe16133310d7dbe115f
smallrussian <julian.shniter@gmail.com>
2023-05-10 15:17:48 -0500

    added auth ui

--- pages/redirect.tsx
+++ pages/redirect.tsx
@@ -8,8 +8,9 @@ const AuthHandler = () => {
 
   useEffect(() => {
     if (user) {
-      if (user.user_metadata.username) {
+      if (user.user_metadata.username !== null) {
         // If user metadata is filled out, the user has signed in before.
+        console.log('user is signed in');
         router.push('/');
       } else {
         // If user metadata is not filled out, the user is a new sign up.
